68ca1b3233c86866fce859e257912945c46fa0c0
Henry AT <yiperu@gmail.com>
2014-04-26 20:00:23 -0300

    Ahora hemos implementado la visualizacion de las imagenes en los detalles del curso, tener cuidado con la direccion URL que se asigna, verificar su existencia

--- src/com/example/PryReadXMLFromRemoteServer/DetalleActivity.java
+++ src/com/example/PryReadXMLFromRemoteServer/DetalleActivity.java
@@ -2,9 +2,16 @@ package com.example.PryReadXMLFromRemoteServer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,6 +25,11 @@ public class DetalleActivity extends Activity {
     private TextView txtPrecio = null;
     private ImageView imgImagen = null;
     private TextView txtDetalle = null;
+
+    // Declaramos las variables necesarias para trabajar las imagenes:
+    private Bitmap imagenCargada;
+    private String direccionImagen = "http://apps4s.com/android-xml/";
+
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -39,5 +51,24 @@ public class DetalleActivity extends Activity {
         txtTitulo.setText(unCurso.getTitulo());
         txtPrecio.setText(unCurso.getPrecio()+" Dolares");
         txtDetalle.setText(unCurso.getDetalle());
+        // Obtenemos el nombre del archivo de la imagen almacenada en el servidor remoto:
+        direccionImagen = direccionImagen + unCurso.getImagen();
+        descargarImagen(direccionImagen);
+    }
+
+    private void descargarImagen(String dirImagen) {
+        URL imagenURL = null;
+        try {
+            imagenURL = new URL(dirImagen);
+            HttpURLConnection conn = (HttpURLConnection)imagenURL.openConnection();
+            conn.connect();
+            imagenCargada = BitmapFactory.decodeStream(conn.getInputStream());
+            imgImagen.setImageBitmap(imagenCargada);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
