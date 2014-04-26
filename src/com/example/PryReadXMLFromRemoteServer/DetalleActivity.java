f23ac9d961cf9da39f6d8f642d9f21e22a8c76a8
Henry AT <yiperu@gmail.com>
2014-04-26 14:19:03 -0300

    Implementamos los controles necesarios, y un layout de detalles, todos con sus propiedades iniciales devidamente configuradas

---
+++ src/com/example/PryReadXMLFromRemoteServer/DetalleActivity.java
@@ -0,0 +1,20 @@
+package com.example.PryReadXMLFromRemoteServer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Henry AT
+ * Date: 26-04-14
+ * Time: 12:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DetalleActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+
+
+    }
+}
--- src/com/example/PryReadXMLFromRemoteServer/MainActivity.java
+++ src/com/example/PryReadXMLFromRemoteServer/MainActivity.java
@@ -1,15 +1,53 @@
 package com.example.PryReadXMLFromRemoteServer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import java.net.URL;
+
+public class MainActivity extends Activity implements View.OnClickListener{
     /**
      * Called when the activity is first created.
      */
+    private EditText editTextUrl = null;
+    private Button buttonSend = null;
+    private ListView listViewDatos=null;
+    // Datos necesario para obtener datos del WS
+    private URL url=null;
+    private String[] titulares=null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        //------------------------------
+        editTextUrl = (EditText)findViewById(R.id.idEdtUrl);
+        buttonSend = (Button)findViewById(R.id.idBtnSend);
+        listViewDatos = (ListView)findViewById(R.id.idLstvDatos);
+
+        buttonSend.setOnClickListener(this);
+
+
+        url = new URL();
+        leerXML();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.idBtnSend:
+                Intent i = new Intent(this,DetalleActivity.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
     }
 }
